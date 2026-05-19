b457c26b6f4a1ae1dad733217c21a4884bd97fa5
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-19 14:50:19 +0900

    refactor

--- c7/alds1_6_a/src/main.rs
+++ c7/alds1_6_a/src/main.rs
@@ -3,20 +3,24 @@
 fn solve(_values: &[usize]) -> Vec<usize> {
     let &max = _values.iter().max().unwrap();
 
-    let mut counts: Vec<usize> = (0..=max).map(|_| 0).collect();
+    let mut counts: Vec<usize> = vec![0; max + 1];
 
+    // index が value で、値が個数
     for &v in _values {
         counts[v] += 1;
     }
 
-    let mut sum_count: Vec<usize> = (0..=max).map(|_| 0).collect();
+    let mut sum_count: Vec<usize> = vec![0; max + 1];
 
+    // ある値までの要素の個数を計算しておくことで、インデックスとなる。
     for (index, &count) in counts.iter().enumerate() {
         sum_count[index] = count + if index > 0 { sum_count[index - 1] } else { 0 };
     }
 
-    let mut sorted: Vec<usize> = (0.._values.len()).map(|_| 0).collect();
+    let mut sorted: Vec<usize> = vec![0; _values.len()];
 
+    // 逆順に取り出すことで安定ソートとしている。
+    // ただ、今回は key と value が同じのため、区別がない。
     for &v in _values.iter().rev() {
         sorted[sum_count[v] - 1] = v;
         sum_count[v] -= 1;
